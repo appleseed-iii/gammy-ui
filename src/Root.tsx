9548427326065b1a617833e8ec5267d31151da43
appleseed-iii <appleseed-iii@protonmail.com>
2022-12-10 06:52:02 -0600

    remove deprecated chain imports from wagmi

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,7 +6,8 @@ import { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "src/App";
 import theme from "src/styles/theme";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
@@ -17,8 +18,8 @@ import { publicProvider } from "wagmi/providers/public";
 
 export const { chains, provider } = configureChains(
   [
-    { ...chain.mainnet, rpcUrls: { default: "https://rpc.ankr.com/eth" } },
-    { ...chain.goerli, rpcUrls: { default: "https://rpc.ankr.com/eth_goerli" } },
+    { ...mainnet, rpcUrls: { default: "https://rpc.ankr.com/eth" } },
+    { ...goerli, rpcUrls: { default: "https://rpc.ankr.com/eth_goerli" } },
   ],
   [
     jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default }) }),
--- src/constants.ts
+++ src/constants.ts
@@ -1,19 +1,20 @@
-import { Chain, chain } from "wagmi";
+import { Chain } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
 
-export const availableChains: Chain[] = [chain.mainnet, chain.goerli];
+export const availableChains: Chain[] = [mainnet, goerli];
 
 interface IAddresses {
   [key: string]: string;
 }
 
 export const GAMMY_MINTER: IAddresses = {
-  [chain.goerli.id]: "0x3B15D749869f99069a8dEb2C44507aEb2e0511d9",
-  [chain.mainnet.id]: "0x2668E84980d0F121F0C01382Fd3f6A72ef44Cfdb",
+  [goerli.id]: "0x3B15D749869f99069a8dEb2C44507aEb2e0511d9",
+  [mainnet.id]: "0x2668E84980d0F121F0C01382Fd3f6A72ef44Cfdb",
 };
 
 export const GOHM_ADDRESSES: IAddresses = {
-  [chain.goerli.id]: "0xC1863141dc1861122d5410fB5973951c82871d98",
-  [chain.mainnet.id]: "0x0ab87046fBb341D058F17CBC4c1133F25a20a52f",
+  [goerli.id]: "0xC1863141dc1861122d5410fB5973951c82871d98",
+  [mainnet.id]: "0x0ab87046fBb341D058F17CBC4c1133F25a20a52f",
 };
 
 export interface EthersError extends Error {
